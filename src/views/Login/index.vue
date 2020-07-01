32d5d7f7ebd42620f4724a9b28b54845e9dd29ae
heazeel <1520800291@qq.com>
2020-07-02 01:15:28 +0800

    创建vue项目，配置全局样式，路由，element-ui

---
+++ src/views/Login/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="login">
+        this is login page!
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+#login{
+    height: 100vh;
+    background-color:#344a5f;
+}
+</style>
